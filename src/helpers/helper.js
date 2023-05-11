9c1823eccb51b21fba4854331306ea08056efcb4
Nadiia Palaida <n.palaida@gmail.com>
2023-05-11 16:25:19 +0300

    Adding dist

--- src/helpers/helper.js
+++
@@ -1,9 +0,0 @@
-export function readFileImage(file, domEl) {
-    if(file && domEl) {
-        const reader = new FileReader()
-        reader.readAsDataURL(file);
-        reader.onload = function (e) {
-           domEl.src = e.target.result
-        };
-    }
-}
